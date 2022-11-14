122404e8c329f5849bb622f04c84ca5cbfdaade1
Monica <monicadolcemascolo@gmail.com>
2022-11-14 11:47:38 -0500

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
 import React from "react";
-import PortfolioContainer from "./components/PortfolioContainer";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import AboutMe from "./components/AboutMe";
+import Portfolio from "./components/Portfolio";
+import Resume from "./components/Resume";
+import Contact from "./components/Contact";
 
-const App = () => <PortfolioContainer />;
 
-export default App;
+
+function App() {
+    return (
+    <>
+        <Navbar />
+        <Header />
+        <AboutMe />
+        <Portfolio />
+        <Resume />
+        <Contact />
+    </>
+    );
+}
+
+export default App
