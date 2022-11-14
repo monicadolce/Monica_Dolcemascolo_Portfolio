ec20c8f5d17f88c56c0cd72d09ad51496230ea23
Monica <monicadolcemascolo@gmail.com>
2022-11-14 15:45:16 -0500

    changed text in resume.js

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -4,7 +4,7 @@ const Resume = () => {
   return (
     <div className='resume'>
     <div className='d-flex justify-content-center my-5'>
-    <a href='https://drive.google.com/file/d/1tryEB24IYS6-RlvU8Zc6QpT0tIJ12j1P/view?usp=share_link'><h1 className='resume-text'>Take a look at my Resume</h1></a>
+    <a href='https://drive.google.com/file/d/1tryEB24IYS6-RlvU8Zc6QpT0tIJ12j1P/view?usp=share_link'><h1 className='resume-text'>Go to my Resume</h1></a>
     </div>
 </div>
 
