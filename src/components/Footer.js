7ae021f9153739cb1309f4228ff036d56c00a25a
Monica <monicadolcemascolo@gmail.com>
2022-11-14 14:58:39 -0500

    working on footer.js and app.css to render icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,39 +1,30 @@
-import React from 'react'
+import React from 'react';
+import {
+    TwitterShareButton,
+    TwitterIcon,
+    LinkedinShareButton,
+    LinkedinIcon
+} from 'react-share';
+
 
 const Footer = () => {
     return (
         <div className='footer'>
-            <div className='container'>
-                <div className='row'>
-                    <div className='col-lg-4 col-md-6 col-sm-6'>
-                        <div className='d-flex'>
-                            <p>Hello</p>
-                        </div>
-                        <div className='d-flex'>
-                            <a href='tel 555'>555</a>
-                        </div>
-                        <div className='d-flex'>
-                            <p>monica@gmail.com</p>
-                        </div>
+            <div className='text-center'>
+                <div className='container'>
+                    <div className='d-flex justify-content-center'>
+                        < TwitterShareButton
+                            url={'https://twitter.com/MonicaDolcemas2'}
+                        >
+                            <TwitterIcon className='mx-3' size={36} />
+                        </TwitterShareButton>
+                        < LinkedinShareButton
+                            url={'https://www.linkedin.com/in/monica-d-a3213921b/'}
+                        >
+                            <LinkedinIcon className='mx-3' size={36} />
+                        </LinkedinShareButton>
                     </div>
-                    <div className='col-lg-3 col-md-2 col-sm-6'>
-                        <div className='row'>
-                            <div className='col'>
-                                <a className='footer-nav'>Home</a>
-                                <br />
-                                <a className='footer-nav'>About Me</a>
-                                <br />
-                                <a className='footer-nav'>Services</a>
-                            </div>
-                            <div className='col'>
-                                <a className='footer-nav'>Experience</a>
-                                <br />
-                                <a className='footer-nav'>Portfolio</a>
-                                <br />
-                                <a className='footer-nav'>Contact</a>
-                            </div>
-                        </div>
-                    </div>
+                    <p className='pt-3 text-center'>© 2022 Monica's Portfolio Copyright </p>
                 </div>
             </div>
         </div>
