8ec48104e84ec99e47a1cff6ff6f5fd15d6ea045
Monica <monicadolcemascolo@gmail.com>
2022-11-14 12:59:04 -0500

    created footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+        <div className='container'>
+            <div className='row'>
+                <div className='col-lg-4 col-md-6 col-sm-6'>
+                    <div className='d-flex'>
+                        <p>Hello</p>
+                    </div>
+                    <div className='d-flex'>
+                        <a href='tel 555'>555</a>
+                    </div>
+                    <div className='d-flex'>
+                        <p>monica@gmail.com</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
