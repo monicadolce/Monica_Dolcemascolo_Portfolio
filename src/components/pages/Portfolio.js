ee9de62416fb065a410b993ef62d165109578ee7
Monica <monicadolcemascolo@gmail.com>
2022-11-17 11:03:43 -0500

    working on portfolio component, adding more apps

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -3,12 +3,14 @@ import nosql from '../../images/nosql.gif';
 import note_taker from '../../images/note_taker.png';
 import scheduler from '../../images/scheduler.gif';
 import weather from '../../images/weather.png';
+import furrever from '../../images/furrever.gif';
+import horiseon from '../../images/horiseon.png';
 
 export default function Portfolio () {
     return (
         <div className='portfolio-wrapper'>
             <div className='container'>
-                <h1 className='text-uppercase text-center py-5'>Portfolio</h1>
+                <h1 className='text-uppercase text-center py-6'>Portfolio</h1>
                 <div className='image-box-wrapper row row-cols-auto justify-content-center'>
                     <div className='portfolio-image-box'>
                         <img className='portfolio-image' src={nosql} alt='nosql project' />
@@ -21,6 +23,12 @@ export default function Portfolio () {
                     </div>
                     <div className='portfolio-image-box'>
                         <img className='portfolio-image' src={weather} alt='weather project' />
+                    </div>
+                    <div className='portfolio-image-box'>
+                        <img className='portfolio-image' src={furrever} alt='furrever project' />
+                    </div>
+                    <div className='portfolio-image-box'>
+                        <img className='portfolio-image' src={horiseon} alt='horiseon project' />
                     </div>
                 </div>
             </div>
