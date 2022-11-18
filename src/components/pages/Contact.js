2020371d184f22f438487db168eb220df3a981d2
Monica <monicadolcemascolo@gmail.com>
2022-11-18 13:59:43 -0500

    working on contact styling

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,45 +1,47 @@
 // import React, { useState } from "react";
 import React, { useRef } from 'react';
 import emailjs from '@emailjs/browser';
-// import { useForm } from "react-hook-form";
-
 
 export default function Contact() {
+
     const form = useRef();
-  
+
     const sendEmail = (e) => {
-      e.preventDefault();
-  
-      emailjs.sendForm('service_ID', 'template_ID', form.current, 'aZEN1SwBnpDTsiKn8')
-        .then((result) => {
-            console.log(result.text);
-        }, (error) => {
-            console.log(error.text);
-        });
+        e.preventDefault();
+
+        emailjs.sendForm('service_ID', 'template_ID', form.current, 'aZEN1SwBnpDTsiKn8')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
     };
-  
     return (
-      <form ref={form} onSubmit={sendEmail}>
-        <label>Name</label>
-        <input type="text" name="user_name" />
-        <label>Email</label>
-        <input type="email" name="user_email" />
-        <label>Message</label>
-        <textarea name="message" />
-        <input type="submit" value="Send" />
-      </form>
+        <div className='contact'>
+            <div className='text-center'>
+                <h1>Contact Me</h1>
+                <div className='container'>
+                    <div className='vstack gap-3'>
+                    <form ref={form} onSubmit={sendEmail}>
+                        <div>
+                        <label>Name</label>
+                        <input type="text" name="name" />
+                        </div>
+                        <div>
+                        <label>Email</label>
+                        <input type="email" name="email" />
+                        </div>
+                        <div>
+                        <label>Message</label>
+                        <textarea name="message" />
+                        <input type="submit" value="Send" />
+                        </div>
+                    </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     );
-  };
 
-
-
-
-
-
-
-
-
-
-
-
-
+}
